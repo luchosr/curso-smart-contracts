34658fa1e65780af058c3f30b3796f3393f71641
Luciano <luchosr@gmail.com>
2018-07-12 17:20:28 -0300

    added new code improvement

--- test/contracts/SimpleStorage.sol
+++ test/contracts/SimpleStorage.sol
@@ -4,6 +4,9 @@ contract SimpleStorage {
     uint storedData;
 
     function set(uint x) public {
+        require(x<10);
+         //require es una palabra reservada que dice que si no se cumple la condicion el contrato se revierte.
+         // Si una transaccion se revierte no es costo para nadie.
         storedData = x;
     }
 
